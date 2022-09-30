0b4f30a709f2e76f9ad1a13d6af3d0bbccf4e199
Ihor Naidonov <igornaydenov246@gmail.com>
2022-09-30 23:57:12 +0300

    removed unused code

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import { mainTitle } from './components/mainTitle/mainTitle';
 import { affectedNumber } from './components/affectedNumber/affectedNumber';
 import { affectedType } from './components/affectedType/affectedType';
 
-import data from './data/names.json';
-console.log(data);
-
 import './assets/styles/index.scss';
 
 (() => {
