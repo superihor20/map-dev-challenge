500512a2e66c8ef04dafc4cad1733b145a59049b
Ihor Naidonov <igornaydenov246@gmail.com>
2022-10-01 00:58:24 +0300

    some dates functions

---
+++ src/utils/constants/dates.constants.js
@@ -0,0 +1,1 @@
+export const oneDayInMs = 1000 * 60 * 60 * 24;
---
+++ src/utils/helpers/dates.helpers.js
@@ -0,0 +1,48 @@
+import { oneDayInMs } from '../constants/dates.constants';
+
+export const getMs = (date) => {
+  return new Date(date).getTime();
+};
+
+export const getYear = (date) => {
+  return new Date(date).getFullYear();
+};
+
+export const getDate = (date) => {
+  return new Date(date).getDate();
+};
+
+export const getMonth = (date) => {
+  return new Date(date).getMonth() + 1;
+};
+
+export const normalizeDate = (date) => {
+  return `${getYear(date)}-${getMonth(date)}-${getDate(date)}`;
+};
+
+export const getRangeOfDates = (from, to) => {
+  const dates = [];
+  const toInMs = getMs(to);
+  let currInMs = getMs(from);
+
+  do {
+    const normalizedDate = normalizeDate(currInMs);
+
+    dates.push(normalizedDate);
+    currInMs += oneDayInMs;
+  } while (currInMs <= toInMs);
+
+  return dates;
+};
+
+export const subtractDaysFromNowDate = (numberOfDaysToSubstract) => {
+  if (typeof numberOfDaysToSubstract !== 'number') {
+    throw new Error('You should path numbers only');
+  }
+
+  const now = new Date();
+  now.setDate(now.getDate() - numberOfDaysToSubstract);
+  now.setHours(0, 0, 0, 0);
+
+  return now;
+};
