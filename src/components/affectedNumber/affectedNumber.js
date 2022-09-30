536c760a7d3b5a1557395ebbd4d7344748e5d7f4
Ihor Naidonov <igornaydenov246@gmail.com>
2022-09-30 22:46:51 +0300

    affected number styles

--- src/components/affectedNumber/affectedNumber.js
+++ src/components/affectedNumber/affectedNumber.js
@@ -4,5 +4,9 @@ import { elementTypes } from '../../utils/constants/elementTypes';
 import styles from './affectedNumber.scss';
 
 export const affectedNumber = (amount) => {
-  return createComponent({ elementType: elementTypes.h1, child: amount });
+  return createComponent({
+    elementType: elementTypes.h1,
+    child: amount,
+    attributes: { class: styles.affectedNumber },
+  });
 };
