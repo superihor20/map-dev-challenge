4c2d0bfc4af6918c6cc50964fedc3eb2076f3557
Ihor Naidonov <igornaydenov246@gmail.com>
2022-10-01 10:15:46 +0300

    rework get event function

--- src/utils/helpers/getEventsForTheLast100Days.js
+++ src/utils/helpers/getEventsForTheLast100Days.js
@@ -1,11 +1,22 @@
-import { getMs, subtractDaysFromNowDate } from './dates.helpers';
+import { getMs, normalizeDate, subtractDaysFromNowDate } from './dates.helpers';
 
 export const getEventsForTheLast100Days = (events) => {
   const dateOneHundredDaysAgoInMs = getMs(subtractDaysFromNowDate(100));
+  const suitableEvents = [];
 
-  return events.filter(
-    ({ from, till }) =>
-      getMs(from) > dateOneHundredDaysAgoInMs ||
-      getMs(till) > dateOneHundredDaysAgoInMs
-  );
+  for (const event of events) {
+    if (getMs(event.from) > dateOneHundredDaysAgoInMs) {
+      suitableEvents.push({ ...event });
+      continue;
+    }
+
+    if (getMs(event.till) > dateOneHundredDaysAgoInMs) {
+      suitableEvents.push({
+        ...event,
+        from: normalizeDate(dateOneHundredDaysAgoInMs),
+      });
+    }
+  }
+
+  return suitableEvents;
 };
