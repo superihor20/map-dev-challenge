f7fd80fcaf74d909f743d0cd1c1db6f5dbedce22
Ihor Naidonov <superihor20@gmail.com>
2022-10-02 16:01:10 +0300

    animation for numbers

--- src/components/affect/affectedNumber/affectedNumber.js
+++ src/components/affect/affectedNumber/affectedNumber.js
@@ -1,12 +1,17 @@
 import { createComponent } from '../../../lib/createComponent';
 import { elementTypes } from '../../../utils/constants/elementTypes';
+import { animateValue } from '../../../utils/helpers/animateValue';
 
 import './affectedNumber.scss';
 
 export const affectedNumber = (number) => {
-  return createComponent({
+  const element = createComponent({
     elementType: elementTypes.p,
     child: number,
     attributes: { class: 'affectedNumber' },
   });
+
+  animateValue(element, 0, number, 1000);
+
+  return element;
 };
