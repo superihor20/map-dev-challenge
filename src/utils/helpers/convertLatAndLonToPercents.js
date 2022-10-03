9fb4e9283a9518e7d1a12227a0088e303a713913
Ihor Naidonov <superihor20@gmail.com>
2022-10-03 14:44:04 +0300

    added explanation for convertion

--- src/utils/helpers/convertLatAndLonToPercents.js
+++ src/utils/helpers/convertLatAndLonToPercents.js
@@ -5,6 +5,10 @@ import {
   northPoint,
 } from '../constants/extremePoints';
 
+// This function should have 100 not 96 neither 99.
+// Maybe because I took not the most extreme points of latitude and longitude.
+// So I used 96 and 99 instead of 100 for more accurate point placement
+
 export const convertLatAndLonToPercents = (lat, lon) => {
   const pointLatDiff = northPoint.lat - lat;
   const percLat = 96 - (pointLatDiff / latDiff) * 100;
